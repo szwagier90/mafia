0fc5493e96ffc30398d7d15ec0a421372739aa98
Paweł Szwagierek <szwagier90@gmail.com>
2016-08-31 17:51:14 +0200

    Game constructor

---
+++ src/mafia.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Game:
+    def __init__(self, name, players):
+        self.name = name
+        self.players = players
---
+++ tests/test_MafiaGame.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from src.mafia import Game
+
+
+class TestMafiaGame(unittest.TestCase):
+
+    def test_new_game(self):
+        game = Game(
+            name="NewGame",
+            players=["Player1", "Player2"],
+        )
+
+if __name__ == '__main__':
+    unittest.main()
