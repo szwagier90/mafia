03fde43e5eea2f68b97e37983a5b1172735a3506
Paweł Szwagierek <szwagier90@gmail.com>
2016-09-01 14:24:06 +0200

    Refactor test to extract setUp method

--- tests/test_MafiaGame.py
+++ tests/test_MafiaGame.py
@@ -8,21 +8,20 @@ from src.mafia import Game
 
 class TestMafiaGame(unittest.TestCase):
 
+    def setUp(self):
+        self.test_name = 'testname'
+        self.game = Game(self.test_name)
+
     def test_new_game_name_correctyly_set(self):
-        test_name = 'testname'
-
-        self.game = Game(test_name)
-
-        self.assertEqual(self.game.name, test_name)
+        self.assertEqual(self.game.name, self.test_name)
 
     def test_game_name_changed(self):
-        test_name = 'testname'
         changed_name = 'testname2'
 
-        self.game = Game(test_name)
         self.game.set_name(changed_name)
 
         self.assertEqual(self.game.name, changed_name)
+
 
 if __name__ == '__main__':
     unittest.main()
