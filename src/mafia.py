713fc83c8c1c0bf756c7bed514db1fd9a9be11d5
Szwagier <szwagier90@gmail.com>
2017-03-09 15:26:14 +0000

    Shuffle list of players on new game

--- src/mafia.py
+++ src/mafia.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import random
 
 class Game:
     def __init__(self, name, players):
         self.name = name
         self.players = players
+
+    def new_game(self, shuffle=random.shuffle):
+        shuffle(self.players)
--- tests/test_MafiaGame.py
+++ tests/test_MafiaGame.py
@@ -5,14 +5,20 @@ import unittest
 
 from src.mafia import Game
 
+from mock import Mock, MagicMock
 
 class TestMafiaGame(unittest.TestCase):
-
     def test_new_game(self):
+        random_mock = Mock()
+        random_mock.shuffle = MagicMock()
+
         game = Game(
             name="NewGame",
             players=["Player1", "Player2"],
         )
+
+        game.new_game(shuffle=random_mock.shuffle)
+        random_mock.shuffle.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
